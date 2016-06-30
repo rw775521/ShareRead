a5a033b52be2f19b67b450b1a1aa1c1c5b811e7e
任为 <renwei@renweideMacBook-Pro.local>
2016-06-30 21:36:46 +0800

    add ShareRead

---
+++ ShareRead/ShareRead/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  ShareRead
+//
+//  Created by 任为 on 16/6/30.
+//  Copyright © 2016年 Reeve. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ ShareRead/ShareRead/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  ShareRead
+//
+//  Created by 任为 on 16/6/30.
+//  Copyright © 2016年 Reeve. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
